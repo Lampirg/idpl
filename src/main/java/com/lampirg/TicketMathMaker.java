31d6dccdfbe2ba2942448221e84475b7d3dcd995
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-08-23 18:04:15 +0300

    Create TicketMathMaker.java

---
+++ src/main/java/com/lampirg/TicketMathMaker.java
@@ -0,0 +1,56 @@
+package com.lampirg;
+
+import com.lampirg.json.Ticket;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class TicketMathMaker {
+
+    public static Result countVvoAndTlv(List<Ticket> tickets) {
+        Map<String, Duration> minTimeForCarrier = new HashMap<>();
+        Map<Integer, Integer> pricesCount = new HashMap<>();
+        for (Ticket ticket : tickets) {
+            if (!fromVvoToTlv(ticket))
+                continue;
+            minTimeForCarrier.merge(
+                    ticket.getCarrier(),
+                    Duration.between(ticket.getDepartureDateTime(), ticket.getArrivalDateTime()),
+                    TicketMathMaker::min
+            );
+            pricesCount.merge(
+                    ticket.getPrice(),
+                    1,
+                    Integer::sum
+            );
+        }
+        double div = pricesCount.entrySet().stream()
+                .collect(
+                        Collectors.teeing(
+                                collectMedian(),
+                                Collectors.summarizingInt(entry -> entry.getKey() * entry.getValue()),
+                                (median, average) -> average.getAverage()
+                        )
+                );
+        return new Result(minTimeForCarrier, div);
+    }
+
+    private static boolean fromVvoToTlv(Ticket ticket) {
+        return ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV");
+    }
+
+    private static Duration min(Duration oldVal, Duration newVal) {
+        if (oldVal.compareTo(newVal) < 0)
+            return oldVal;
+        return newVal;
+    }
+
+    private static Collector<Map.Entry<Integer, Integer>, Object, Integer> collectMedian() {
+        return Collectors.collectingAndThen(
+                Collectors.maxBy(Comparator.comparingInt(Map.Entry::getValue)),
+                res -> res.orElseThrow().getValue()
+        );
+    }
+}
