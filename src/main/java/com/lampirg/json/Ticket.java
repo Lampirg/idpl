92c7c156c59be3c34155bd6ffe87d24da8f6c154
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-08-23 18:04:09 +0300

    Create JSON classes

---
+++ src/main/java/com/lampirg/json/Ticket.java
@@ -0,0 +1,42 @@
+package com.lampirg.json;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+public class Ticket {
+    private String origin;
+    @JsonProperty("origin_name")
+    private String originName;
+    private String destination;
+    @JsonProperty("destination_name")
+    private String destinationName;
+    @JsonProperty("departure_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    private LocalDate departureDate;
+    @JsonProperty("departure_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    private LocalTime departureTime;
+    @JsonProperty("arrival_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    private LocalDate arrivalDate;
+    @JsonProperty("arrival_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    private LocalTime arrivalTime;
+    private String carrier;
+    private int stops;
+    private int price;
+
+    public LocalDateTime getDepartureDateTime() {
+        return LocalDateTime.of(departureDate, departureTime);
+    }
+
+    public LocalDateTime getArrivalDateTime() {
+        return LocalDateTime.of(arrivalDate, arrivalTime);
+    }
+}
---
+++ src/main/java/com/lampirg/json/TicketList.java
@@ -0,0 +1,10 @@
+package com.lampirg.json;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TicketList {
+    List<Ticket> tickets;
+}
