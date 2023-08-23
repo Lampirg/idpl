2a3aa5c9343af78efa417f25598b4f0e92269c6a
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-08-23 18:17:01 +0300

    Refactor: rename method in Main class

--- src/main/java/com/lampirg/Main.java
+++ src/main/java/com/lampirg/Main.java
@@ -10,14 +10,14 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        TicketList ticketList = getTicketList();
+        TicketList ticketList = readTicketList();
         Result result = TicketMathMaker.countVvoAndTlv(ticketList.getTickets());
         result.getMin().forEach((key, val) -> System.out.println(key + ": " + val.toMinutes() + " minutes"));
         System.out.println(result.getDiv());
     }
 
     @SneakyThrows
-    private static TicketList getTicketList() {
+    private static TicketList readTicketList() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.readValue(new File("src/main/resources/tickets.json"), TicketList.class);
