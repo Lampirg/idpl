9f48a4798840b0db2a27e985c5470dc35bb3c858
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-08-23 18:03:53 +0300

    Create Main.java

---
+++ src/main/java/com/lampirg/Main.java
@@ -0,0 +1,26 @@
+package com.lampirg;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.lampirg.json.TicketList;
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TicketList ticketList = getTicketList();
+        Result result = TicketMathMaker.countVvoAndTlv(ticketList.getTickets());
+        result.getMin().forEach((key, val) -> System.out.println(key + ": " + val.toMinutes() + " minutes"));
+        System.out.println(result.getDiv());
+    }
+
+    @SneakyThrows
+    private static TicketList getTicketList() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper.readValue(new File("src/main/resources/tickets.json"), TicketList.class);
+    }
+}
